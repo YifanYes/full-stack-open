8fc26a0958dd94e54b53955c0c59cbdd6268f088
Yifan <yifanyemontpe@gmail.com>
2025-05-23 12:56:16 +0200

    add part1 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
+const Statistics = ({ good, neutral, bad }) => {
+  const isGreaterThanZero = good + neutral + bad
+
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {isGreaterThanZero ? ((good - bad) / (good + neutral + bad)).toFixed(2) : 'no data'}</p>
+      <p>positive {isGreaterThanZero ? Math.round((good / (good + neutral + bad)) * 100, 2) : 'no data'} %</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const isGreaterThanZero = good + neutral + bad
 
   return (
     <>
@@ -15,13 +29,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {isGreaterThanZero ? Math.round((good - bad) / (good + neutral + bad), 2) : 'no data'}</p>
-      <p>positive {isGreaterThanZero ? `${Math.round((good / (good + neutral + bad)) * 100, 2)} %` : 'no data'}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
