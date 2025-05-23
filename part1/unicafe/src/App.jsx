6827a6c5369d694909d696c0339b5343742c9cea
Yifan <yifanyemontpe@gmail.com>
2025-05-23 13:02:28 +0200

    add part1 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,26 +3,30 @@ import { useState } from 'react'
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
 const StatisticLine = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
-  const isGreaterThanZero = good + neutral + bad
+  const total = good + neutral + bad
+  const isGreaterThanZero = total > 0
 
   return (
     <>
+      <h2>statistics</h2>
       {isGreaterThanZero ? (
-        <>
-          <h2>statistics</h2>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={good + neutral + bad} />
-          <StatisticLine text="average" value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
-          <StatisticLine text="positive" value={Math.round((good / (good + neutral + bad)) * 100, 2)} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={((good - bad) / total).toFixed(2)} />
+            <StatisticLine text="positive" value={((good / total) * 100).toFixed(2) + ' %'} />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
