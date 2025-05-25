080456c759658ef4f9c865a18f55bdff6b8a3604
Yifan <yifanyemontpe@gmail.com>
2025-05-25 13:50:14 +0200

    add part2 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newPerson, setNewPerson] = useState({ name: '', number: '' })
+  const [searchText, setSearchText] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -16,21 +22,31 @@ const App = () => {
     setNewPerson({ name: '', number: '' })
   }
 
+  const handleFilterPersons = (event) => {
+    setSearchText(event.target.value)
+    setPersons(persons.filter((person) => person.name.toLowerCase().includes(searchText.toLowerCase())))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={searchText} onChange={handleFilterPersons} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:
           <input
-            value={newPerson.name || ''}
+            value={newPerson.name}
             onChange={(event) => setNewPerson({ ...newPerson, name: event.target.value })}
           />
         </div>
         <div>
           number:
           <input
-            value={newPerson.number || ''}
+            value={newPerson.number}
             onChange={(event) => setNewPerson({ ...newPerson, number: event.target.value })}
           />
         </div>
