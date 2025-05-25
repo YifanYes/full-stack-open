cb8014770aed010dca13f4cde3aaa4e489a4547c
Yifan <yifanyemontpe@gmail.com>
2025-05-25 13:19:43 +0200

    add part2 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,11 @@
+import Content from './Content'
+import Header from './Header'
+
+const Course = ({ course }) => (
+  <>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </>
+)
+
+export default Course
