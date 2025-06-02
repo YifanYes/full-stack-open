859ba09251ee000e1eed62a9d7fd1b756d8d3892
Yifan <yifanyemontpe@gmail.com>
2025-06-02 11:46:58 +0200

    add part3 3.5 and 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,11 +28,11 @@ let phonebook = [
 ]
 
 app.get('/info', (_req, res) => {
-  res.status(200).send(`Phonebook has info for ${phonebook.length} people <br/> ${new Date()}`)
+  return res.status(200).send(`Phonebook has info for ${phonebook.length} people <br/> ${new Date()}`)
 })
 
 app.get('/api/persons', (_req, res) => {
-  res.status(200).json(phonebook)
+  return res.status(200).json(phonebook)
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -55,6 +55,30 @@ app.delete('/api/persons/:id', (req, res) => {
   phonebook = phonebook.filter((person) => person.id !== personId)
 
   return res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required' })
+  }
+  if (!number) {
+    return res.status(400).json({ error: 'Number is required' })
+  }
+
+  const existingName = phonebook.find((person) => person.name === name)
+  if (existingName) {
+    return res.status(400).json({ error: 'Name must be unique' })
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000).toString(),
+    name,
+    number
+  }
+  phonebook.push(newPerson)
+
+  return res.status(200).json(newPerson)
 })
 
 const PORT = 3001
