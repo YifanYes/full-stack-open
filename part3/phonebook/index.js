5a237d719cf46ef916f46d36c82f73966e3e16b6
Yifan <yifanyemontpe@gmail.com>
2025-06-02 11:56:51 +0200

    add part3 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,11 @@ import morgan from 'morgan'
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let phonebook = [
   {
