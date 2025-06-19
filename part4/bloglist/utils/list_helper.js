e1b9a2fd2cc2818a817d4f55bcbfc4b0902f5420
Yifan <yifanyemontpe@gmail.com>
2025-06-19 17:38:11 +0200

    add part4 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,8 @@
 export const dummy = (blogs) => 1
 
 export const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0)
+
+export const favoriteBlog = (blogs) => {
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
+  return sortedBlogs[0]
+}
